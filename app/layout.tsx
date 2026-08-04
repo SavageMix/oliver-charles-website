ba7af5bfb6058d9c9ae4e583715dbf2587ea2b94
Ollie <oliver@olivercharles.construction>
2026-08-04 17:35:31 +0100

    Add Google Tag Manager (GTM-M5DBJN5T)

--- app/layout.tsx
+++ app/layout.tsx
@@ -210,6 +210,15 @@ export default function RootLayout({
 
           gtag('config', 'AW-18189121818');
         `}} />
+
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-M5DBJN5T');
+        `}} />
         
         {/* Schema.org Structured Data */}
         <script
@@ -218,6 +227,15 @@ export default function RootLayout({
         />
       </head>
       <body className="antialiased font-body">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M5DBJN5T"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Navigation />
         {children}
         <Footer />
