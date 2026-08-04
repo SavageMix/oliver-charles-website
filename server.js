68911981602154213cf6c30bc45af1456da18f75
Ollie <oliver@olivercharles.construction>
2026-08-04 18:19:33 +0100

    Allow Google Analytics regional endpoints in CSP

--- server.js
+++ server.js
@@ -59,11 +59,11 @@ app.use(helmet({
     directives: {
       defaultSrc: ["'self'"],
       styleSrc: ["'self'", "'unsafe-inline'"],
-      scriptSrc: ["'self'", "'unsafe-inline'", "https://www.googletagmanager.com", "https://www.google-analytics.com", "https://googleads.g.doubleclick.net", "https://www.googleadservices.com"],
-      imgSrc: ["'self'", "data:", "https:", "https://www.google-analytics.com", "https://www.googletagmanager.com", "https://googleads.g.doubleclick.net", "https://www.googleadservices.com"],
+      scriptSrc: ["'self'", "'unsafe-inline'", "https://www.googletagmanager.com", "https://*.googletagmanager.com", "https://www.google-analytics.com", "https://*.google-analytics.com", "https://*.analytics.google.com", "https://googleads.g.doubleclick.net", "https://www.googleadservices.com"],
+      imgSrc: ["'self'", "data:", "https:", "https://www.google-analytics.com", "https://*.google-analytics.com", "https://*.analytics.google.com", "https://www.googletagmanager.com", "https://*.googletagmanager.com", "https://googleads.g.doubleclick.net", "https://www.googleadservices.com"],
       connectSrc: isDev
-        ? ["'self'", "http://localhost:3001", "https://maps.googleapis.com", "https://www.google-analytics.com", "https://www.googletagmanager.com", "https://ad.doubleclick.net", "https://www.google.com", "https://googleads.g.doubleclick.net", "https://www.googleadservices.com", "https://www.olivercharlesgardendesign.co.uk"]
-        : ["'self'", "https://maps.googleapis.com", "https://www.google-analytics.com", "https://www.googletagmanager.com", "https://ad.doubleclick.net", "https://www.google.com", "https://googleads.g.doubleclick.net", "https://www.googleadservices.com", "https://www.olivercharlesgardendesign.co.uk"],
+        ? ["'self'", "http://localhost:3001", "https://maps.googleapis.com", "https://www.google-analytics.com", "https://*.google-analytics.com", "https://*.analytics.google.com", "https://www.googletagmanager.com", "https://*.googletagmanager.com", "https://ad.doubleclick.net", "https://www.google.com", "https://googleads.g.doubleclick.net", "https://www.googleadservices.com", "https://www.olivercharlesgardendesign.co.uk"]
+        : ["'self'", "https://maps.googleapis.com", "https://www.google-analytics.com", "https://*.google-analytics.com", "https://*.analytics.google.com", "https://www.googletagmanager.com", "https://*.googletagmanager.com", "https://ad.doubleclick.net", "https://www.google.com", "https://googleads.g.doubleclick.net", "https://www.googleadservices.com", "https://www.olivercharlesgardendesign.co.uk"],
       frameSrc: ["'self'", "https://www.google.com", "https://www.google.co.uk", "https://googleads.g.doubleclick.net"],
     },
   },
